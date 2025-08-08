24959375954685ffef567fe0947d6de89d60dd49
tobi <onatadetobi212@gmail.com>
2025-08-09 00:44:37 +0100

    deployment 3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1','https://portfolio-engine.pxxl.click']
+ALLOWED_HOSTS = ['127.0.0.1','portfolio-engine.pxxl.click']
 
 
 # Application definition
